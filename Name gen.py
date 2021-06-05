5b9cd7c069071cd79093f8b2573c25767f1589a5
Sean Connelly <you@example.com>
2021-06-05 18:05:09 -0400

    first commit

---
+++ Name gen.py
@@ -0,0 +1,10 @@
+# Purpose: to generate names for various purposes including filling out rosters of mercenaries
+# Features: Name, race, specialty, quirks
+
+#Individual = class
+#   race - weighted list favoring humans and droids
+#   name - pull from list appropriate to race
+#   Role - list of specialties, but should cap certain numbers of personel like not too many pilots
+#   quirks - from list, but quirks should also be able to pull from list of race and specialty specific quirks
+#       quirks can have variables that can be filled, like music variable which pulls a random genre from a list
+#   preferred weapon(s) - Dependant on role
